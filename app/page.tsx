9ffa388e28b33d3ffdfef8f4868762f6522c53f3
Swaleh Mhina <mhinasw@gmail.com>
2025-09-01 09:52:45 +0300

    Use the Client in the Dashboard Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import Image from "next/image";
+import { createClient } from "./_utils/supabase-server";
+import { redirect } from "next/navigation";
+
 
 export default function Home() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
   return (
     <main>
       <h1 className="text-4xl font-bold">Dashboard</h1>
-      <p>This is the main dashboard content area</p>
+      <p className="mt-2 text-lg">Welcome back, {user.email}!</p>
+      <p>This is your personalized dashboard content area</p>
     </main>
   );
 }
