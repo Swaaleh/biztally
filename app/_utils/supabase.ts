918f56aa97d6dd8d516d1938124744e866660092
Swaleh Mhina <mhinasw@gmail.com>
2025-09-04 10:24:33 +0300

    Passing the Database type to Supabase client

--- app/_utils/supabase.ts
+++ app/_utils/supabase.ts
@@ -1,5 +1,6 @@
 // app/_utils/supabase.ts
 import { createClient } from '@supabase/supabase-js';
+import { Database } from './supabase/db-types';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabasePublishableKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
@@ -8,4 +9,7 @@ if (!supabaseUrl || !supabasePublishableKey) {
   throw new Error('Missing Supabase environment variables');
 }
 
-export const supabase = createClient(supabaseUrl, supabasePublishableKey);
+export const supabase = createClient<Database>(
+  supabaseUrl, 
+  supabasePublishableKey
+);
