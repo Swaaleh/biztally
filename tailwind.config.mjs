fb4a4d03d5bf3e8341fb0a6047f8310fa32b1ab3
Swaleh Mhina <mhinasw@gmail.com>
2025-09-15 15:18:11 +0300

    Addd tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,15 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        success: 'rgb(var(--color-success) / <alpha-value>)',
+        warning: 'rgb(var(--color-warning) / <alpha-value>)',
+        danger: 'rgb(var(--color-danger) / <alpha-value>)',
+      }
+    },
   },
   plugins: [],
 };
